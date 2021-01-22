e8d91ca74c4e9dd03c17904c12c169ca54023f37
zwimpee <zwimpee@angelo.edu>
2021-01-22 10:27:01 -0600

    Update training.py

--- training.py
+++ training.py
@@ -1,14 +1,15 @@
-"""
-Author: Zachary Wimpee
-
-Description: Script for training a model and saving 
-             the state_dict for the best weights.
-             
-Note: Should figure out how to implement command line
-      arguments to generalize the training script to 
-      any desired architecture and specify training 
-      parameters. 
-"""
+###############################################################################
+# CXResnet model training
+#
+# Author: Zachary Wimpee
+#
+# This file contains the procedures for training a model using one of the 
+# networks defined in the architecture.py file. It can be executed using the
+# default command line arguments, but many values can be specified by the user
+# allowing for increased flexibility and versatility.
+# 
+#
+###############################################################################
 import argparse
 from pathlib import Path
 import json
@@ -26,12 +27,10 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
-#import utils
-#from utils import n_random_samples, show_predictions
 from preprocessing import rmtree
 
 from architecture import ResidualNet, ConvNet
-from sklearn.metrics import balanced_accuracy_score
+
 
 classes = ('COVID','Normal','Pneumonia')
 
@@ -56,14 +55,16 @@ def n_random_samples(dataset, n_samples = 100):
 
 def prediction_probs(model, images):
     # Get model outputs for input images.
-    outputs = model(images)
+    model.eval()
+    with torch.no_grad():
+        outputs = model(images)
+        
+        # Get index of predicted class.
+        preds = outputs.argmax(dim=1).tolist()
     
-    # Get index of predicted class.
-    preds = outputs.argmax(dim=1).tolist()
-    
-    # Get the probability of the predictions.
-    probs = [F.softmax(outputs[i],dim=0)[preds[i]].item() for i in range(len(preds))]
-    
+        # Get the probability of the predictions.
+        probs = [F.softmax(outputs[i],dim=0)[preds[i]].item() for i in range(len(preds))]
+        
     return preds, probs
 
 def show_predictions(model, images, labels, mean, std):
@@ -87,18 +88,20 @@ def show_predictions(model, images, labels, mean, std):
                     color=("green" if preds[i]==labels[i].item() else "red"))
     return fig
 
+
 # Printing out the results after each epoch.
 def epoch_end(epoch, train_scores, val_scores):
     print(
         'Epoch [{}], train_loss: {:.4f}, train_acc: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}'
-        .format(epoch+1,
-                train_scores['train_loss'],train_scores['train_acc'],
-                val_scores['val_loss'],val_scores['val_acc']))
+        .format(epoch+1,train_scores['train_loss'],train_scores['train_acc'],val_scores['val_loss'],val_scores['val_acc']))
     
         
 # Training function for each epoch.
-def epoch_phase(model, criterion, optimizer, loader, device, phase):
+def epoch_phase(model,criterion,optimizer,
+                loader,device, phase,
+                epoch, running_loss, writer):
     # Initialize values to track.
+    i = epoch * len(loader) # current number of minibatches for phase
     epoch_loss = 0.0
     preds = torch.tensor([],device=device)
     targets = torch.tensor([],device=device)
@@ -111,14 +114,16 @@ def epoch_phase(model, criterion, optimizer, loader, device, phase):
         
     # Loop over dataloader batches.
     for inputs, labels in loader:
+        # Increment minibatch count.
+        i += 1
+        
         # Get input and label batch, append labels to targets. 
         inputs = inputs.to(device,non_blocking=True)
         labels = labels.to(device,non_blocking=True)
         targets = torch.cat((targets,labels),dim=0)
 
         # Zero the optimizer gradients.
-        #optimizer.zero_grad()
-        model.zero_grad()
+        optimizer.zero_grad()
         
         with torch.set_grad_enabled(phase == 'train'):
             # Get outputs and calculate the predicted class label indices.
@@ -132,23 +137,32 @@ def epoch_phase(model, criterion, optimizer, loader, device, phase):
                 # Do backpropagation and update the model weights.
                 loss.backward()
                 optimizer.step()
-            
-        
 
+        # Increment running loss
+        running_loss += loss.item()
+        
         # Deavgerage and increment the epoch training loss.
         epoch_loss += loss.item() * inputs.size(0)
         
+        # Log the phase's running loss every 10 minibatches.
+        if i % 10 == 9:
+            writer.add_scalar('Loss/Batches/{}'.format(phase), running_loss / 10, i)
+            running_loss = 0.0
+            
+        
+        
     # Calculate the training loss and accuracy score.
     epoch_loss = epoch_loss / len(loader.dataset)
-    epoch_acc = balanced_accuracy_score(targets.cpu(),preds.cpu())
+    
+    counts = torch.unique(targets, return_counts=True)[1]
+    epoch_acc = torch.tensor([torch.logical_and(torch.eq(targets, i),torch.eq(preds, i)).sum() / counts[i] for i in range(3)]).sum() / 3
 
     # Save epoch scores in dict.
     epoch_scores = {'{}_loss'.format(phase): epoch_loss,
-                    '{}_acc'.format(phase): epoch_acc}
+                    '{}_acc'.format(phase): epoch_acc,
+                    '{}_rl'.format(phase): running_loss}
     
     return epoch_scores
-
-
 
 def main(args):
     # Assign device as GPU if available, else CPU.
@@ -178,7 +192,7 @@ def main(args):
     trainset = torchvision.datasets.ImageFolder(
         traindir, transforms.Compose([
             transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
+            transforms.RandomHorizontalFlip(p=0.33),
             transforms.ToTensor(),
             transforms.Normalize(mean, std)
         ]))
@@ -192,11 +206,11 @@ def main(args):
         ]))
     
     
-    train_dl = torch.utils.data.DataLoader(trainset,batch_size=args.batch_size,shuffle=True,pin_memory=args.memory_pinning, num_workers=args.workers)
+    train_dl = torch.utils.data.DataLoader(trainset,batch_size=args.batch_size,
+                                           shuffle=True,pin_memory=args.memory_pinning,num_workers=args.workers)
     
-    val_dl = torch.utils.data.DataLoader(valset,batch_size=2*args.batch_size,shuffle=False,pin_memory=args.memory_pinning, num_workers=args.workers)
-    
-    
+    val_dl = torch.utils.data.DataLoader(valset,batch_size=2*args.batch_size,
+                                         shuffle=False,pin_memory=args.memory_pinning, num_workers=args.workers)
     
     # Initialize model, loss function, and optimizer.
     if args.arch == "res":
@@ -204,7 +218,7 @@ def main(args):
     else:
         model = ConvNet().to(device)
         
-    criterion = nn.CrossEntropyLoss().to(device)
+    criterion = nn.CrossEntropyLoss()
     
     if args.optim == "adam":
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
@@ -224,16 +238,22 @@ def main(args):
     
     # Train for specified number of epochs.
     best_acc = 0.0
-    best_weights = copy.deepcopy(model.state_dict())
+    train_rl = 0.0
+    val_rl = 0.0
+    #best_weights = copy.deepcopy(model.state_dict())
     for epoch in range(args.epochs):
-        train_scores = epoch_phase(model, criterion, optimizer, loader=train_dl, device=device, phase='train')
-        val_scores = epoch_phase(model, criterion, optimizer, loader=val_dl, device=device, phase='val')
+        train_scores = epoch_phase(model, criterion, optimizer, loader=train_dl, device=device, phase='train', epoch=epoch, running_loss=train_rl, writer=writer)
+        val_scores = epoch_phase(model, criterion, optimizer, loader=val_dl, device=device, phase='val', epoch=epoch, running_loss=val_rl, writer=writer)
+        
+        # Update the running losses.
+        train_rl = train_scores['train_rl']
+        val_rl = val_scores['val_rl']
         
         epoch_end(epoch, train_scores, val_scores)
         
         # Write loss and accuracy scores to TensorBoard.
-        writer.add_scalar('Loss/train', train_scores['train_loss'], epoch)
-        writer.add_scalar('Loss/val', val_scores['val_loss'], epoch)
+        writer.add_scalar('Loss/Epoch/train', train_scores['train_loss'], epoch)
+        writer.add_scalar('Loss/Epoch/val', val_scores['val_loss'], epoch)
         writer.add_scalar('Accuracy/train', train_scores['train_acc'], epoch)
         writer.add_scalar('Accuracy/val', val_scores['val_acc'], epoch)
         
@@ -252,13 +272,13 @@ def main(args):
                     labels,mean,std),global_step=epoch)
             writer.flush()
         
-        if val_scores['val_acc'] > best_acc:
+        """if val_scores['val_acc'] > best_acc:
             best_acc = val_scores['val_acc']
-            best_weights = copy.deepcopy(model.state_dict())
+            best_weights = copy.deepcopy(model.state_dict())"""
     
             
     # Save the trained model to specified path.
-    model.load_state_dict(best_weights)
+    #model.load_state_dict(best_weights)
     PATH = '/'.join(['models',args.save_path])
     torch.save(model.state_dict(), PATH)
     writer.close()
@@ -272,19 +292,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Model training script")
     parser.add_argument("-d","--data", type=str, default="data", help="name of data directory")
     parser.add_argument("-b","--batch-size", type=int, default=128, help="dataloader batch size")
-    
-    #parser.add_argument("--memory-pinning", type=bool, default=True, help="copies tensors to CUDA pinned memory when true")
     parser.add_argument("-p", "--memory-pinning", action="store_true", help="copy tensors to CUDA pinned memory")
-    
-    parser.add_argument("-w","--workers",type=int, default=0, help="number of subprocesses for data loading")
+    parser.add_argument("-w","--workers",type=int, default=4, help="number of subprocesses for data loading")
     parser.add_argument("-a","--arch", type=str, default="res", choices = ["res","conv"], help="select model architecture")
     parser.add_argument("-o","--optim", type=str, default="adam", choices=["adam","sgd"],help="select optimizer algorithm")
     parser.add_argument("-l","--lr", type=float, default=0.001,help="optimizer learning rate")
     parser.add_argument("-m","--momentum", type=float, help="momentum parameter value, required when not using adam optimizer")
     parser.add_argument("-e","--epochs",type=int, default=10,help="number of epochs to train")
     parser.add_argument("-s","--save-path",type=str,default="trained_model.pt", help="path to save trained model")
-    
-    #parser.add_argument("--tb-clear", type=bool, default=False, help="clears all tensorboard logs")
     parser.add_argument("-c","--tb-clear", action="store_true", help="clears all tensorboard logs")
     parser.add_argument("-t","--tb-folder", type=str, default="cxresnet", help="tensorboard child directory for model being trained")
     args = parser.parse_args()
